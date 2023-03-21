b0a62fdda27b231a6af8fa46df1ffabd7692da31
Josh Thor <wunjosh@gmail.com>
2023-03-21 21:16:51 +0800

    chore: WIP

--- sources/brains/ft_think.c
+++ sources/brains/ft_think.c
@@ -2,11 +2,11 @@
 
 void	ft_think(t_philo *philo)
 {
-	// grab forks
+	// if first time
 	if (philo->to_die == 0)
 		philo->to_die = gettime_ms() + philo->base->data->t_2die;
 	if (philo->base->death_flag == 1)
 		return ;
 	print_state(philo, K_THINK);
-	
+	if (philo->id % 2 != 0)
 }
