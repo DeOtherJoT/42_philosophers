9fbb775eb0fae058ad4ef8848ca6b400fd6fb6f6
Josh Thor <wunjosh@gmail.com>
2023-03-21 21:15:43 +0800

    feat: Added starve() function
    chore: Renamed mutex

--- sources/utils/utils2.c
+++ sources/utils/utils2.c
@@ -2,7 +2,7 @@
 
 void	print_state(t_philo *philo, int flag)
 {
-	pthread_mutex_lock(&(philo->base->state_mtx));
+	pthread_mutex_lock(&(philo->base->write_mtx));
 	if (flag == K_EAT)
 		printf("%s%ld %i %s", C_Y, gettime_ms(), philo->id, "is eating");
 	else if (flag == K_THINK)
@@ -10,7 +10,21 @@ void	print_state(t_philo *philo, int flag)
 	else if (flag == K_SLEEP)
 		printf("%s%ld %i %s", C_B, gettime_ms(), philo->id, "is sleeping");
 	else
-		printf("%s%ld %i %s", C_R, gettime_ms(), philo->id, "is dead");
-	printf("%s\n", C_N);
-	pthread_mutex_unlock(&(philo->base->state_mtx));
+	{
+		if (philo->base->death_flag == 0)
+			printf("%s%ld %i %s", C_R, gettime_ms(), philo->id, "is dead");
+	}
+	printf("%s\n", C_NC);
+	pthread_mutex_unlock(&(philo->base->write_mtx));
+}
+
+void	starve(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->base->death_mtx));
+	if (philo->base->death_flag == 0)
+	{
+		philo->base->death_flag = 1;
+		print_state(philo, K_DEATH);
+	}
+	pthread_mutex_unlock(&(philo->base->death_mtx));
 }
