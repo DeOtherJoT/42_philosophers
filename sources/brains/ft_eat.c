e809dcd91f5876ed89c0d8f5593e011136d79978
Josh Thor <wunjosh@gmail.com>
2023-03-22 20:19:19 +0800

    feat: completed WIP

--- sources/brains/ft_eat.c
+++ sources/brains/ft_eat.c
@@ -9,15 +9,35 @@ void	ft_eat(t_philo *philo)
 		return ;
 	time_now = gettime_ms();
 	if (time_now > philo->to_die)
+	{
 		starve(philo);
-	print_state(philo, K_EAT);
+		return ;
+	}
+	print_state(philo, C_Y, "is eating");
+	philo->num_ate += 1;
 	unlock_forks(philo, philo->l_fork, philo->r_fork);
 	philo->to_die += philo->base->data->t_2die;
-	ft_sleep(philo->base->data->t_2eat);
+	my_usleep(philo, philo->base->data->t_2eat);
+}
+
+void	starve(t_philo *philo)
+{
+	pthread_mutex_lock(&(philo->base->death_mtx));
+	if (philo->base->death_flag == 0)
+	{
+		philo->base->death_flag = 1;
+		unlock_forks(philo, philo->l_fork, philo->r_fork);
+		print_death(philo);
+	}
+	pthread_mutex_unlock(&(philo->base->death_mtx));
 }
 
 void	unlock_forks(t_philo *philo, int l_fork, int r_fork)
 {
 	pthread_mutex_unlock(&(philo->base->fork_mtx[l_fork]));
 	pthread_mutex_unlock(&(philo->base->fork_mtx[r_fork]));
+	pthread_mutex_lock(&(philo->base->fstate_mtx));
+	philo->base->fork_stat[l_fork] = 0;
+	philo->base->fork_stat[r_fork] = 0;
+	pthread_mutex_lock(&(philo->base->fstate_mtx));
 }
