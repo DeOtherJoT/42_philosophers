493824b5962283129466af575de426e7d04792f8
Josh Thor <wunjosh@gmail.com>
2023-03-23 21:36:45 +0800

    chore: added a newline to the end of the line.

--- sources/utils/utils2.c
+++ sources/utils/utils2.c
@@ -4,13 +4,13 @@ void	print_state(t_philo *philo, char *colour, char *str)
 {
 	pthread_mutex_lock(&(philo->base->write_mtx));
 	if (philo->base->death_flag == 0)
-		printf("%s%zu %zu %s%s", colour, gettime_ms(), philo->id, str, C_NC);
+		printf("%s%zu %zu %s%s\n", colour, gettime_ms(), philo->id, str, C_NC);
 	pthread_mutex_unlock(&(philo->base->write_mtx));
 }
 
 void	print_death(t_philo *philo)
 {
 	pthread_mutex_lock(&(philo->base->write_mtx));
-	printf("%s%zu %zu %s%s", C_R, gettime_ms(), philo->id, "died", C_NC);
+	printf("%s%zu %zu %s%s\n", C_R, gettime_ms(), philo->id, "died", C_NC);
 	pthread_mutex_unlock(&(philo->base->write_mtx));
 }
