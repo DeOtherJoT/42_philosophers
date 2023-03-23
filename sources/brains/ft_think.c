665f17f2ed51cf5110ca5a41a5a9ab65a2f1e267
Josh Thor <wunjosh@gmail.com>
2023-03-23 21:34:04 +0800

    chore: reduced odd wait time
    refactor: removed time_now variable
    
    refactor: shortened grab_fork()

--- sources/brains/ft_think.c
+++ sources/brains/ft_think.c
@@ -9,7 +9,7 @@ void	ft_think(t_philo *philo)
 		return ;
 	print_state(philo, C_G, "is thinking");
 	if (philo->id % 2 != 0)
-		usleep((philo->base->data->t_2eat) * 0.9);
+		usleep((philo->base->data->t_2eat) * 0.5);
 	queue_forks(philo, philo->l_fork, philo->r_fork);
 }
 
@@ -20,35 +20,31 @@ void	queue_forks(t_philo *philo, int l_fork, int r_fork)
 	if (grab_fork(philo, philo->base, r_fork) == 0)
 	{
 		pthread_mutex_unlock(&(philo->base->fork_mtx[l_fork]));
+		pthread_mutex_lock(&(philo->base->fstate_mtx));
+		philo->base->fork_stat[l_fork] = 0;
+		pthread_mutex_unlock(&(philo->base->fstate_mtx));
 		return ;
 	}
 }
 
 int	grab_fork(t_philo *philo, t_base *base, int fork)
 {
-	size_t	time_now;
-
-	while (1)
+	while (gettime_ms() < philo->to_die)
 	{
-		time_now = gettime_ms();
 		if (base->death_flag == 1)
 			return (0);
-		if (time_now > philo->to_die)
-		{
-			pthread_mutex_lock(&(base->death_mtx));
-			base->death_flag = 1;
-			print_death(philo);
-			pthread_mutex_unlock(&(base->death_mtx));
-			return (0);
-		}
+		pthread_mutex_lock(&(base->fstate_mtx));
 		if (base->fork_stat[fork] == 0)
 		{
-			pthread_mutex_lock(&(base->fstate_mtx));
 			base->fork_stat[fork] = 1;
 			pthread_mutex_lock(&(base->fork_mtx[fork]));
+			print_state(philo, C_W, "has taken a fork");
 			pthread_mutex_unlock(&(base->fstate_mtx));
 			return (1);
 		}
+		pthread_mutex_unlock(&(base->fstate_mtx));
 		usleep(500);
 	}
+	starve(philo);
+	return (0);
 }
